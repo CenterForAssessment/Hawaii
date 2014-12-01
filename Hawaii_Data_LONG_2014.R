3c69b76c976c2e5b01c16a604dd39648c1a1db14
dbetebenner <dbetebenner@gmail.com>
2014-12-01 11:12:55 -0500

    Minor modifications to Hawaii_Data_LONG_2014

--- Hawaii_Data_LONG_2014.R
+++ Hawaii_Data_LONG_2014.R
@@ -12,7 +12,7 @@ require(data.table)
 ### Load tab delimited data
 
 Hawaii_Data_LONG_2014 <- as.data.table(read.csv("Data/Base_Files/Hawaii_Data_LONG_2014.txt", sep="|", stringsAsFactors=FALSE))
-
+SCHOOL_DATA_ANNUAL_2014_Lookup <- fread("Data/Base_Files/Corrected_and_Updated_Metadata/SCHOOL_DATA_ANNUAL_2013_Table_1.csv")
 
 ### Tidy up data
 
@@ -44,27 +44,27 @@ levels(Hawaii_Data_LONG_2014$Complex)[29] <- "McKinley"
 Hawaii_Data_LONG_2014$Complex.Area <- as.factor(Hawaii_Data_LONG_2014$Complex.Area)
 levels(Hawaii_Data_LONG_2014$Complex.Area)[8] <- "Hilo-Laupahoehoe-Waiakea"
 
-Hawaii_Data_LONG_2014$GENDER <- as.factor(Hawaii_Data_LONG_2014$GENDER)
+Hawaii_Data_LONG_2014$Sex <- as.factor(Hawaii_Data_LONG_2014$Sex)
 Hawaii_Data_LONG_2014$ELL_STATUS_MULTILEVEL <- as.factor(Hawaii_Data_LONG_2014$ELL_STATUS_MULTILEVEL)
-Hawaii_Data_LONG_2014$SCHOOL_NAME <- as.factor(Hawaii_Data_LONG_2014$SCHOOL_NAME)
-Hawaii_Data_LONG_2014$DISTRICT_NAME <- as.factor(Hawaii_Data_LONG_2014$DISTRICT_NAME)
+Hawaii_Data_LONG_2014$School_Admin_Rollup <- as.factor(Hawaii_Data_LONG_2014$School_Admin_Rollup)
+Hawaii_Data_LONG_2014$District <- as.factor(Hawaii_Data_LONG_2014$District)
 Hawaii_Data_LONG_2014$STATE_ENROLLMENT_STATUS <- as.factor(Hawaii_Data_LONG_2014$STATE_ENROLLMENT_STATUS)
 Hawaii_Data_LONG_2014$SCHOOL_ENROLLMENT_STATUS <- as.factor(Hawaii_Data_LONG_2014$SCHOOL_ENROLLMENT_STATUS)
 Hawaii_Data_LONG_2014$DISTRICT_ENROLLMENT_STATUS <- as.factor(Hawaii_Data_LONG_2014$DISTRICT_ENROLLMENT_STATUS)
 Hawaii_Data_LONG_2014$COMPLEX_ENROLLMENT_STATUS <- as.factor(Hawaii_Data_LONG_2014$COMPLEX_ENROLLMENT_STATUS)
 Hawaii_Data_LONG_2014$COMPLEX_AREA_ENROLLMENT_STATUS <- as.factor(Hawaii_Data_LONG_2014$COMPLEX_AREA_ENROLLMENT_STATUS)
 
-Hawaii_Data_LONG_2014$FSY_SchCode[Hawaii_Data_LONG_2014$FSY_SchCode="NULL"] <- NA
+Hawaii_Data_LONG_2014$FSY_SchCode[Hawaii_Data_LONG_2014$FSY_SchCode=="NULL"] <- NA
 Hawaii_Data_LONG_2014$FSY_SchCode <- as.integer(Hawaii_Data_LONG_2014$FSY_SchCode)
 
-Hawaii_Data_LONG_2013$HIGH_NEEDS_STATUS_DEMOGRAPHIC <- 
+Hawaii_Data_LONG_2014$HIGH_NEED_STATUS_DEMOGRAPHIC <- 
         factor(2, levels=1:2, labels=c("High Need Status: ELL, Special Education, or Disadvantaged Student", "High Need Status: Non-ELL, Non-Special Education, and Non-Disadvantaged Student"))
-HawaiI_Data_LONG_2013$HIGH_NEEDS_STATUS_DEMOGRAPHIC[HawaiI_Data_LONG_2013$DISADVANTAGED_STATUS=="Disadvantaged: Yes" | HawaiI_Data_LONG_2013$ELL_STATUS=="ELL Status: Yes" | Hawaii_SGP@Data$SPECIAL_EDUCATION_STATUS=="Special Education: Yes"] <- "High Need Status: ELL, Special Education, or Disadvantaged Student"
+Hawaii_Data_LONG_2014$HIGH_NEED_STATUS_DEMOGRAPHIC[Hawaii_Data_LONG_2014$DISADVANTAGED_STATUS=="Disadvantaged: Yes" | Hawaii_Data_LONG_2014$ELL_STATUS=="ELL Status: Yes" | Hawaii_Data_LONG_2014$SPECIAL_EDUCATION_STATUS=="Special Education: Yes"] <- "High Need Status: ELL, Special Education, or Disadvantaged Student"
 
 ### Reorder variables
 
 my.variable.order <- c("VALID_CASE", "Domain", "Year", "Gr", "IDNO", "LName", "FName", "SCode_Admin_Rollup", "School_Admin_Rollup", "FSY_SchCode", "EMH.Level", "DCode", "District", "CCode", "Complex",
-	"CACode", "Complex.Area", "Sex", "ETHNICITY", "HIGH_NEEDS_STATUS_DEMOGRAPHIC", "DOE_Ethnic", "Fed7_Ethnic", "Fed5_Ethnic", "Disadv", "ELL", "ELL_STATUS_MULTILEVEL", "SpEd", 
+	"CACode", "Complex.Area", "Sex", "ETHNICITY", "HIGH_NEED_STATUS_DEMOGRAPHIC", "DOE_Ethnic", "Fed7_Ethnic", "Fed5_Ethnic", "Disadv", "ELL", "ELL_STATUS_MULTILEVEL", "SpEd", 
 	"Migrant", "Scale_Score", "Proficiency_Level", "FSY", "SCHOOL_ENROLLMENT_STATUS", "DISTRICT_ENROLLMENT_STATUS", "COMPLEX_ENROLLMENT_STATUS", "COMPLEX_AREA_ENROLLMENT_STATUS", 
 	"STATE_ENROLLMENT_STATUS") 
 setcolorder(Hawaii_Data_LONG_2014, my.variable.order)
