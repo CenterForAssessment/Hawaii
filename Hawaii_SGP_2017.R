d67003c3d689a3d465606d7035ed97f381dcb0e6
dbetebenner <dbetebenner@gmail.com>
2017-06-01 14:29:14 -0400

    Adding in 2017 scripts

---
+++ Hawaii_SGP_2017.R
@@ -0,0 +1,45 @@
+###########################################################
+###
+### Hawaii SGP Analysis for 2017
+###
+###########################################################
+
+### Load SGP Package
+
+require(SGP)
+
+
+### Load previous SGP object and 2017 data
+
+load("Data/Hawaii_SGP.Rdata")
+load("Data/Hawaii_Data_LONG_2017.Rdata")
+load("Data/Hawaii_Data_LONG_2017_INSTRUCTOR_NUMBER.Rdata")
+
+
+### Load configurations
+
+source("SGP_CONFIG/2017/READING.R")
+source("SGP_CONFIG/2017/MATHEMATICS.R")
+
+HI_CONFIG <- c(READING_2017.config, MATHEMATICS_2017.config)
+
+### Update SGPs
+
+Hawaii_SGP <- updateSGP(
+			Hawaii_SGP,
+			Hawaii_Data_LONG_2017,
+			Hawaii_Data_LONG_2017_INSTRUCTOR_NUMBER,
+			steps=c("prepareSGP", "analyzeSGP", "combineSGP", "summarizeSGP", "outputSGP"),
+			sgp.percentiles.baseline=FALSE,
+			sgp.projections.baseline=FALSE,
+			sgp.projections.lagged.baseline=FALSE,
+			sgp.target.scale.scores=TRUE,
+			save.intermediate.results=FALSE,
+			sgPlot.demo.report=TRUE,
+			sgp.config=HI_CONFIG,
+			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=2,  PROJECTIONS=2, LAGGED_PROJECTIONS=2, SUMMARY=2, GA_PLOTS=2, SG_PLOTS=1)))
+
+
+### Save results
+
+save(Hawaii_SGP, file="Data/Hawaii_SGP.Rdata")
---
+++ SGP_CONFIG/2017/READING.R
@@ -0,0 +1,10 @@
+READING_2017.config <- list(
+        READING.2017 = list(
+                sgp.content.areas=rep('READING', 6),
+                sgp.panel.years=as.character(2012:2017),
+                sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('3', '4', '5', '6', '7'), c('3', '4', '5', '6', '7', '8'))),
+        READING.2017 = list(
+                sgp.content.areas=rep('READING', 4),
+                sgp.panel.years=as.character(c(2012:2014, 2017)),
+                sgp.grade.sequences=list(c('6', '7', '8', '11')),
+		sgp.projection.grade.sequences=as.list("NO_PROJECTIONS")))
