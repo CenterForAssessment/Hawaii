bc66dc38a505e09f99c507b53706dba9f77e760a
Damian Betebenner <dbetebenner@nciea.org>
2018-08-02 14:18:34 -0400

    Adding in 2018 SGP scripts

---
+++ Hawaii_SGP_2018.R
@@ -0,0 +1,45 @@
+###########################################################
+###
+### Hawaii SGP Analysis for 2018
+###
+###########################################################
+
+### Load SGP Package
+
+require(SGP)
+
+
+### Load previous SGP object and 2018 data
+
+load("Data/Hawaii_SGP.Rdata")
+load("Data/Hawaii_Data_LONG_2018.Rdata")
+#load("Data/Hawaii_Data_LONG_2018_INSTRUCTOR_NUMBER.Rdata")
+
+
+### Load configurations
+
+source("SGP_CONFIG/2018/READING.R")
+source("SGP_CONFIG/2018/MATHEMATICS.R")
+
+HI_CONFIG <- c(READING_2018.config, MATHEMATICS_2018.config)
+
+### Update SGPs
+
+Hawaii_SGP <- updateSGP(
+			Hawaii_SGP,
+			Hawaii_Data_LONG_2018,
+#			Hawaii_Data_LONG_2018_INSTRUCTOR_NUMBER,
+			steps=c("prepareSGP", "analyzeSGP", "combineSGP", "summarizeSGP", "outputSGP"),
+			sgp.percentiles.baseline=FALSE,
+			sgp.projections.baseline=FALSE,
+			sgp.projections.lagged.baseline=FALSE,
+			sgp.target.scale.scores=TRUE,
+			save.intermediate.results=FALSE,
+			sgPlot.demo.report=TRUE,
+			sgp.config=HI_CONFIG,
+			parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=2,  PROJECTIONS=2, LAGGED_PROJECTIONS=2, SUMMARY=2, GA_PLOTS=2, SG_PLOTS=1)))
+
+
+### Save results
+
+save(Hawaii_SGP, file="Data/Hawaii_SGP.Rdata")
---
+++ SGP_CONFIG/2018/READING.R
@@ -0,0 +1,10 @@
+READING_2018.config <- list(
+        READING.2018 = list(
+                sgp.content.areas=rep('READING', 6),
+                sgp.panel.years=as.character(2013:2018),
+                sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('3', '4', '5', '6', '7'), c('3', '4', '5', '6', '7', '8'))),
+        READING.2018 = list(
+                sgp.content.areas=rep('READING', 4),
+                sgp.panel.years=as.character(c(2013:2015, 2018)),
+                sgp.grade.sequences=list(c('6', '7', '8', '11')),
+		sgp.projection.grade.sequences=as.list("NO_PROJECTIONS")))
