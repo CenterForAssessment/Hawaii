b46cfa3007d3e6c3dd3b2a0e6938dc421e375253
Damian Betebenner <dbetebenner@nciea.org>
2019-08-26 07:43:58 -0400

    Adding in 2019 configurations

---
+++ SGP_CONFIG/2019/MATHEMATICS.R
@@ -0,0 +1,10 @@
+MATHEMATICS_2019.config <- list(
+        MATHEMATICS.2019 = list(
+                sgp.content.areas=rep('MATHEMATICS', 6),
+                sgp.panel.years=as.character(2014:2019),
+                sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('3', '4', '5', '6', '7'), c('3', '4', '5', '6', '7', '8'))),
+        MATHEMATICS.2019 = list(
+                sgp.content.areas=rep('MATHEMATICS', 4),
+                sgp.panel.years=as.character(c(2014:2016, 2019)),
+                sgp.grade.sequences=list(c('6', '7', '8', '11')),
+		sgp.projection.grade.sequences=as.list("NO_PROJECTIONS")))
---
+++ SGP_CONFIG/2019/READING.R
@@ -0,0 +1,10 @@
+READING_2019.config <- list(
+        READING.2019 = list(
+                sgp.content.areas=rep('READING', 6),
+                sgp.panel.years=as.character(2014:2019),
+                sgp.grade.sequences=list(c('3', '4'), c('3', '4', '5'), c('3', '4', '5', '6'), c('3', '4', '5', '6', '7'), c('3', '4', '5', '6', '7', '8'))),
+        READING.2019 = list(
+                sgp.content.areas=rep('READING', 4),
+                sgp.panel.years=as.character(c(2014:2016, 2019)),
+                sgp.grade.sequences=list(c('6', '7', '8', '11')),
+		sgp.projection.grade.sequences=as.list("NO_PROJECTIONS")))
