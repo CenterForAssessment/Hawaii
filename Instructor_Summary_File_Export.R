02fa05d238d861e40d8091dd25db9bfe7d609075
Damian Betebenner <dbetebenner@nciea.org>
2020-01-16 13:14:14 -0500

    Tidying up repo

--- Instructor_Summary_File_Export.R
+++ Instructor_Summary_File_Export.R
@@ -4,6 +4,7 @@
 ###
 ##################################################################################
 
+
 ### Load packages
 
 require(SGP)
@@ -21,53 +22,53 @@ load("Data/Hawaii_SGP.Rdata")
 
 summary.data <- Hawaii_SGP@Summary$SCHOOL_NUMBER[['SCHOOL_NUMBER__INSTRUCTOR_NUMBER__INSTRUCTOR_ENROLLMENT_STATUS']]
 summary.data <- summary.data[INSTRUCTOR_ENROLLMENT_STATUS=="Enrolled Instructor: Yes"]
-write.table(summary.table, file="School_by_Instructor.txt", row.names=FALSE, na="", quote=FALSE, sep="|")
+fwrite(summary.data, file="School_by_Instructor.txt", quote=FALSE, sep="|")
 
 
 ## School_by_ContentArea_by_Instructor.txt
 
 summary.data <- Hawaii_SGP@Summary$SCHOOL_NUMBER[['SCHOOL_NUMBER__INSTRUCTOR_NUMBER__CONTENT_AREA__INSTRUCTOR_ENROLLMENT_STATUS']]
 summary.data <- summary.data[INSTRUCTOR_ENROLLMENT_STATUS=="Enrolled Instructor: Yes"]
-write.table(summary.table, file="School_by_ContentArea_by_Instructor.txt", row.names=FALSE, na="", quote=FALSE, sep="|")
+fwrite(summary.data, file="School_by_ContentArea_by_Instructor.txt", quote=FALSE, sep="|")
 
 
 ## School_by_Year_by_ContentArea_by_Instructor.txt
 
 summary.data <- Hawaii_SGP@Summary$SCHOOL_NUMBER[['SCHOOL_NUMBER__INSTRUCTOR_NUMBER__CONTENT_AREA__YEAR__INSTRUCTOR_ENROLLMENT_STATUS']]
 summary.data <- summary.data[INSTRUCTOR_ENROLLMENT_STATUS=="Enrolled Instructor: Yes"]
-write.table(summary.table, file="School_by_Year_by_ContentArea_by_Instructor.txt", row.names=FALSE, na="", quote=FALSE, sep="|")
+fwrite(summary.data, file="School_by_Year_by_ContentArea_by_Instructor.txt", quote=FALSE, sep="|")
 
 
 ## School_by_Year_by_Instructor.txt
 
 summary.data <- Hawaii_SGP@Summary$SCHOOL_NUMBER[['SCHOOL_NUMBER__INSTRUCTOR_NUMBER__YEAR__INSTRUCTOR_ENROLLMENT_STATUS']]
 summary.data <- summary.data[INSTRUCTOR_ENROLLMENT_STATUS=="Enrolled Instructor: Yes"]
-write.table(summary.table, file="School_by_Year_by_Instructor.txt", row.names=FALSE, na="", quote=FALSE, sep="|")
+fwrite(summary.data, file="School_by_Year_by_Instructor.txt", quote=FALSE, sep="|")
 
 
 ## State_by_Instructor.txt
 
-summary.data <- Hawaii_SGP@Summary$SCHOOL_NUMBER[['STATE__INSTRUCTOR_NUMBER__INSTRUCTOR_ENROLLMENT_STATUS']]
+summary.data <- Hawaii_SGP@Summary$STATE[['STATE__INSTRUCTOR_NUMBER__INSTRUCTOR_ENROLLMENT_STATUS']]
 summary.data <- summary.data[INSTRUCTOR_ENROLLMENT_STATUS=="Enrolled Instructor: Yes"]
-write.table(summary.table, file="State_by_Instructor.txt", row.names=FALSE, na="", quote=FALSE, sep="|")
+fwrite(summary.data, file="State_by_Instructor.txt", quote=FALSE, sep="|")
 
 
 ## State_by_ContentArea_by_Instructor.txt
 
-summary.data <- Hawaii_SGP@Summary$SCHOOL_NUMBER[['STATE__INSTRUCTOR_NUMBER__CONTENT_AREA__INSTRUCTOR_ENROLLMENT_STATUS']]
+summary.data <- Hawaii_SGP@Summary$STATE[['STATE__INSTRUCTOR_NUMBER__CONTENT_AREA__INSTRUCTOR_ENROLLMENT_STATUS']]
 summary.data <- summary.data[INSTRUCTOR_ENROLLMENT_STATUS=="Enrolled Instructor: Yes"]
-write.table(summary.table, file="State_by_ContentArea_by_Instructor.txt", row.names=FALSE, na="", quote=FALSE, sep="|")
+fwrite(summary.data, file="State_by_ContentArea_by_Instructor.txt", quote=FALSE, sep="|")
 
 
 ## State_by_Year_by_ContentArea_by_Instructor.txt
 
-summary.data <- Hawaii_SGP@Summary$SCHOOL_NUMBER[['STATE__INSTRUCTOR_NUMBER__CONTENT_AREA__YEAR__INSTRUCTOR_ENROLLMENT_STATUS']]
+summary.data <- Hawaii_SGP@Summary$STATE[['STATE__INSTRUCTOR_NUMBER__CONTENT_AREA__YEAR__INSTRUCTOR_ENROLLMENT_STATUS']]
 summary.data <- summary.data[INSTRUCTOR_ENROLLMENT_STATUS=="Enrolled Instructor: Yes"]
-write.table(summary.table, file="State_by_Year_by_ContentArea_by_Instructor.txt", row.names=FALSE, na="", quote=FALSE, sep="|")
+fwrite(summary.data, file="State_by_Year_by_ContentArea_by_Instructor.txt", quote=FALSE, sep="|")
 
 
 ## State_by_Year_by_Instructor.txt
 
-summary.data <- Hawaii_SGP@Summary$SCHOOL_NUMBER[['STATE__INSTRUCTOR_NUMBER__YEAR__INSTRUCTOR_ENROLLMENT_STATUS']]
+summary.data <- Hawaii_SGP@Summary$STATE[['STATE__INSTRUCTOR_NUMBER__YEAR__INSTRUCTOR_ENROLLMENT_STATUS']]
 summary.data <- summary.data[INSTRUCTOR_ENROLLMENT_STATUS=="Enrolled Instructor: Yes"]
-write.table(summary.table, file="State_by_Year_by_Instructor.txt", row.names=FALSE, na="", quote=FALSE, sep="|")
+fwrite(summary.data, file="State_by_Year_by_Instructor.txt", quote=FALSE, sep="|")
---
+++ School_Summary_File_Export.R
@@ -0,0 +1,38 @@
+##################################################################################
+###
+### R script to create Instructor level summary files
+###
+##################################################################################
+
+### Load packages
+
+require(SGP)
+require(data.table)
+
+
+### Load Hawaii SGP object
+
+load("Data/Hawaii_SGP.Rdata")
+
+
+### Create summary files
+
+## School_Number_FSY_by_Content_Area_by_Year.txt
+
+summary.data <- Hawaii_SGP@Summary$SCHOOL_NUMBER_FSY[['SCHOOL_NUMBER_FSY__CONTENT_AREA__YEAR__SCHOOL_FSY_ENROLLMENT_STATUS']]
+summary.data <- summary.data[SCHOOL_FSY_ENROLLMENT_STATUS=="Enrolled School: Yes"]
+fwrite(summary.data, file="School_Number_FSY_by_Content_Area_by_Year.txt", quote=FALSE, sep="|")
+
+
+## School_Number_by_Content_Area_by_Year.txt
+
+summary.data <- Hawaii_SGP@Summary$SCHOOL_NUMBER[['SCHOOL_NUMBER__CONTENT_AREA__YEAR__SCHOOL_ENROLLMENT_STATUS']]
+summary.data <- summary.data[SCHOOL_ENROLLMENT_STATUS=="Enrolled School: Yes"]
+fwrite(summary.data, file="School_Number_by_Content_Area_by_Year.txt", quote=FALSE, sep="|")
+
+
+## School_Number_by_EMH_by_Content_Area_by_Year.txt
+
+summary.data <- Hawaii_SGP@Summary$SCHOOL_NUMBER[['SCHOOL_NUMBER__EMH_LEVEL__CONTENT_AREA__YEAR__SCHOOL_ENROLLMENT_STATUS']]
+summary.data <- summary.data[SCHOOL_ENROLLMENT_STATUS=="Enrolled School: Yes"]
+fwrite(summary.data, file="School_Number_by_EMH_by_Content_Area_by_Year.txt", quote=FALSE, sep="|")
