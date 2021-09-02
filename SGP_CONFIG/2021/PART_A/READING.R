2d71ece92d141d6f4490cb4956fa7d7a6496cf9e
Damian Betebenner <dbetebenner@nciea.org>
2021-09-02 06:29:38 -0400

    Updating READING configurations

--- SGP_CONFIG/2021/PART_A/READING.R
+++ SGP_CONFIG/2021/PART_A/READING.R
@@ -7,4 +7,8 @@ READING_2021.config <- list(
                  sgp.content.areas = c('READING', 'READING', 'READING'),
                  sgp.panel.years = c('2018', '2019', '2021'),
                  sgp.grade.sequences = list(c('3', '5'), c('3', '4', '6'), c('4', '5', '7'), c('5', '6', '8')))
+     READING.2021 = list(
+                 sgp.content.areas = c('READING', 'READING'),
+                 sgp.panel.years = c('2017', '2018', '2021'),
+                 sgp.grade.sequences = list(c('7', '8', '11')))
 )
