382fea3c8b5ee55bd55a87a63a0145a57c7f7ecd
Damian Betebenner <dbetebenner@nciea.org>
2024-07-20 13:01:31 -0400

    Adding in 2024 scripts and configurations

---
+++ Hawaii_SGP_2024.R
@@ -0,0 +1,69 @@
+######################################################################################
+###                                                                                ###
+###                Hawaii SGP analyses for 2024                                    ###
+###                                                                                ###
+######################################################################################
+
+###   Load packages
+require(SGP)
+require(SGPmatrices)
+
+###   Load data
+load("Data/Hawaii_SGP.Rdata")
+load("Data/Hawaii_Data_LONG_2024.Rdata")
+
+###   Add Baseline matrices to SGPstateData
+SGPstateData <- addBaselineMatrices("HI", "2024")
+
+###   Read in SGP Configuration Scripts and Combine
+source("SGP_CONFIG/2024/READING.R")
+source("SGP_CONFIG/2024/MATHEMATICS.R")
+
+HI_CONFIG <- c(READING_2024.config, MATHEMATICS_2024.config)
+
+### Parameters
+parallel.config <- list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4))
+
+#####
+###   Run updateSGP analysis
+#####
+
+Hawaii_SGP <- updateSGP(
+        what_sgp_object = Hawaii_SGP,
+        with_sgp_data_LONG = Hawaii_Data_LONG_2024,
+        steps = c("prepareSGP", "analyzeSGP", "combineSGP", "summarizeSGP", "outputSGP"),
+        sgp.config = HI_CONFIG,
+        sgp.percentiles = TRUE,
+        sgp.projections = TRUE,
+        sgp.projections.lagged = TRUE,
+        sgp.percentiles.baseline = TRUE,
+        sgp.projections.baseline = TRUE,
+        sgp.projections.lagged.baseline = TRUE,
+        save.intermediate.results = FALSE,
+        parallel.config = parallel.config
+)
+
+###   Save results
+save(Hawaii_SGP, file="Data/Hawaii_SGP.Rdata")
+
+#' #### 2024 Analyses
+#' 
+#' In the 2024 analyses, we calculate "consecutive-year"
+#' cohort- and baseline-referenced SGPs concurrently for grades 4 through 8
+#' mathematics and ELA. Both SGP analysis versions use two prior scores
+#' (i.e. 2022 and 2023).
+#' 
+#' In the calculation workflow, we again first add the pre-calculated baseline
+#' matrices to `SGPstateData` via the `SGPmatrices` package and load the 2022
+#' configuration scripts to specify the analyses.
+#'
+#' In this part we use the [`updateSGP`](https://www.rdocumentation.org/packages/SGP/versions/2.0-0.0/topics/updateSGP)
+#' function to ***a)*** prepare the `Hawaii_SGP` object used in Part A and add
+#' the cleaned and formatted 2022 data (`prepareSGP`), ***b)*** calculate 2022
+#' consecutive-year cohort- and baseline-referenced SGP estimates and growth
+#' projections (`analyzeSGP` step), ***c)*** merge the results into the master
+#' longitudinal data set (`combineSGP` step), ***d)*** create summary tables
+#' disaggregated at the state, district, complex and school levels
+#' ([`summarizeSGP`](https://www.rdocumentation.org/packages/SGP/versions/2.0-0.0/topics/updateSGP) step)
+#' and ***e)*** save the results in both `.Rdata` and pipe delimited versions
+#' ([`outputSGP`](https://www.rdocumentation.org/packages/SGP/versions/2.0-0.0/topics/outputSGP) step).
